6519dc9f2745de7f33b26e2eb6e16cfe7d28ad1d
Ronal Casani <ronalsistemas@gmail.com>
2026-03-09 12:40:26 -0500

    Proyecto_Base

---
+++ src/main/java/com/rcasani/ApiClienteServiceV1Application.java
@@ -0,0 +1,13 @@
+package com.rcasani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClienteServiceV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiClienteServiceV1Application.class, args);
+    }
+
+}
